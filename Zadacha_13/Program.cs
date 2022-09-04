41cbb03c6c1d6c1ecc042ede968762cde751aadf
Дмитрий <rotweis87@gmail.com>
2022-09-04 11:29:00 +0300

    Добавил решение задачи 13

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void OutThreeNum()
+{
+    Console.Write("Введите целое число ");
+    int a = Convert.ToInt32(Console.ReadLine());   
+        
+    if(a < 100)
+        {
+            Console.WriteLine("Третьего числа нет");
+        }
+    else if(a < 1000) 
+        {
+            int b = a % 10;
+            Console.WriteLine(b);
+        }
+    else
+        {
+            while(a > 999)
+            {
+                a = a / 10;
+            }
+            int b = a % 10;
+            Console.WriteLine(b);
+        }
+}
+
+OutThreeNum();
+OutThreeNum();
+OutThreeNum();
