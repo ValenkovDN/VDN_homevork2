720f4eda85520467eb2452ddd14ecc155ee8fcc6
Дмитрий <rotweis87@gmail.com>
2022-09-04 11:24:49 +0300

    Добавил решение задачи 10

---
+++ Zadacha_10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void PrintToDigit()
+{
+    Console.Write("Введите трехзначное число ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    
+    int b = (a % 100)/10;
+    Console.WriteLine($"Вторая цифра числа {a} это {b}");
+}
+
+PrintToDigit();
+PrintToDigit();
+PrintToDigit();
